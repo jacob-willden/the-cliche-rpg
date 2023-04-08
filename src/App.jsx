dc291fe8204d3771809cb56469809f80f1f0035c
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-04-08 15:30:15 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './bulma.min.css';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className='title'>My App</h1>
+		</>
+	);
+}
+
+export default App;
