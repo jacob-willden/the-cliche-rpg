b50c7d409d862e9e06b3d923dadd0137fadbaf46
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-04-10 16:42:13 -0600

    Overworld markup scaffolding in place

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,25 @@ import './App.css';
 
 function App() {
 	return (
-		<>
-			<h1 className='title'>My App</h1>
-		</>
+		<div id='game'>
+			<button id='options-button' className='button'>Options</button>
+			<button id='inventory-button' className='button'>Inventory</button>
+			<div id='choices-view'>
+				<div id='choices-list'>
+					<button className='button choice'>Foo</button>
+					<button className='button choice'>Bar</button>
+					<button className='button choice'>Baz</button>
+				</div>
+			</div>
+			<div id='reserves'>
+				<output><span id='experience'>0</span> Experience</output>
+				<output><span id='money'>0</span> Gold</output>
+			</div>
+			<div id='primary-textbox' className='card'>
+				<p id='primary-textbox-text'>Hello World!</p>
+				<button className='button next-button'>Next</button>
+			</div>
+		</div>
 	);
 }
 
