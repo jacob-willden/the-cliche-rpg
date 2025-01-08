9c3dad9df7b135955920112a3d0d4c3451ce2376
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2025-01-07 17:49:39 -0700

    Add script licenses table for LibreJS

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://github.com/vitejs/vite/issues/378#issuecomment-768816653
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].js"
+			}
+		}
+	}
 })
